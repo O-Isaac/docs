2b5be11d3d3d11d1798f81e587bdb44441e4f205
O-Isaac <soporte@proyectograndorder.es>
2024-06-01 16:51:12 +0200

    Añade CMS

---
+++ keystatic.config.ts
@@ -0,0 +1,29 @@
+import docs from '@/models/docs';
+
+import { mark } from '@/utils/logo';
+import { config, collection } from '@keystatic/core';
+
+export default config({
+  storage: {
+    kind: 'github',
+    repo: `Proyecto-Grand-Order-Oficial/docs`
+  }, 
+  collections: {
+    docs: collection({
+      label: 'Documentación',
+      slugField: 'title',
+      columns: ['title', 'description'],
+      path: 'src/content/docs/**',
+      format: { contentField: 'content' },
+      entryLayout: "content",
+      schema: docs,
+    }),
+  },
+  ui: {
+    brand: {
+        name: "Proyecto Grand Order",
+        mark
+    },
+  },
+  locale: "es-ES"
+});
