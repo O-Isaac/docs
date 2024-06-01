a1717e3d7a88db1190c4fcbc2bea6a78f4a95dc3
O-Isaac <soporte@proyectograndorder.es>
2024-06-01 17:53:39 +0200

    dynamic images imports

--- src/models/docs.ts
+++ src/models/docs.ts
@@ -3,5 +3,18 @@ import { fields } from "@keystatic/core";
 export default {
     title: fields.slug({ name: { label: 'Titulo' } }),
     description: fields.text({ label: 'Descripción' }),
-    content: fields.mdx({ label: "Cuerpo" }),
+    content: fields.mdx({ 
+        label: "Cuerpo",
+        options: {
+            image: {
+                directory: 'src/assets/images/docs/inline',
+                publicPath: '@assets/images/docs/inline/'
+            }
+        } 
+    }),
+    image: fields.image({
+        label: 'Image',
+        directory: 'src/assets/images/docs/feature',
+        publicPath: '@assets/images/docs/feature/'
+    })
 }
