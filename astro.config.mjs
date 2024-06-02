a089eb048073b614e787a5a51f58f82a4d6361ea
O-Isaac <soporte@proyectograndorder.es>
2024-06-02 12:28:59 +0200

    Add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://v2.proyectograndorder.es",
   devToolbar: {
     enabled: false
   },
