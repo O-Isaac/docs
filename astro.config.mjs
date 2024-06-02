2f87b1bb3eb9316072c94c0b46ffeb947e2ff497
O-Isaac <soporte@proyectograndorder.es>
2024-06-02 12:38:23 +0200

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
 import AutoImport from 'astro-auto-import';
-
 import react from "@astrojs/react";
-import keystatic from '@keystatic/astro'
+import keystatic from '@keystatic/astro';
 import mdx from '@astrojs/mdx';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,41 +15,32 @@ export default defineConfig({
     enabled: false
   },
   output: "hybrid",
-  integrations: [
-    starlight({
-      title: "Proyecto Grand Order",
-      customCss: ["./src/tailwind.css"],
-      social: {
-        github: "https://github.com/withastro/starlight"
-      },
-      components: {
-        PageFrame: "@/layouts/main.astro",
-        Hero: "@/components/core/hero.astro",
-        Head: "@/components/core/head.astro"
-      },
-      sidebar: [{
-        label: "Rayshift",
-        autogenerate: {
-          directory: "rayshift"
-        }
-      }, {
-        label: "Instalacion",
-        autogenerate: {
-          directory: "instalacion"
-        }
-      }]
-  }), 
-  tailwind({
+  integrations: [starlight({
+    title: "Proyecto Grand Order",
+    customCss: ["./src/tailwind.css"],
+    social: {
+      github: "https://github.com/withastro/starlight"
+    },
+    components: {
+      PageFrame: "@/layouts/main.astro",
+      Hero: "@/components/core/hero.astro",
+      Head: "@/components/core/head.astro"
+    },
+    sidebar: [{
+      label: "Rayshift",
+      autogenerate: {
+        directory: "rayshift"
+      }
+    }, {
+      label: "Instalacion",
+      autogenerate: {
+        directory: "instalacion"
+      }
+    }]
+  }), tailwind({
     applyBaseStyles: false
-  }),
-  AutoImport({
-    imports: [
-      '@components/extended/Carousel.astro',
-      '@components/extended/Image.astro',
-    ],
-  }),
-  react(),
-  keystatic(),
-  mdx()
-  ]
+  }), AutoImport({
+    imports: ['@components/extended/Carousel.astro', '@components/extended/Image.astro']
+  }), react(), keystatic(), mdx()],
+  adapter: vercel()
 });
